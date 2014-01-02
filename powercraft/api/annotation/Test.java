1f0fac09ebb9d83491ff2727e94891c21ea3c8a1
zeuslightning125 <Zeusoflightning125@gmail.com>
2014-01-02 11:49:41 -0800

    Ignore me

---
+++ powercraft/api/annotation/Test.java
@@ -0,0 +1,24 @@
+package powercraft.api.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author James
+ * I genuinely have no idea how to make annotations
+ * so, this is here for personal example
+ */
+@Documented // Means stuff needs javadoc
+@Target(ElementType.METHOD) // What do you want it to be put over
+@Inherited // Includes classes that the class parsed inherits
+@Retention(value = RetentionPolicy.RUNTIME) // Leave as is
+public @interface Test {
+	@SuppressWarnings("javadoc")
+	String valueOfAnote() default "There can be default values!";
+	@SuppressWarnings("javadoc")
+	int valueOfAnoteInt(); // NOT having a default makes it required
+}
