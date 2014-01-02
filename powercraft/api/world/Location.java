15e654b51d33839f32bf5b46a3550cf3b9382fd8
zeuslightning125 <Zeusoflightning125@gmail.com>
2014-01-02 09:47:01 -0800

    Initial commit

---
+++ powercraft/api/world/Location.java
@@ -0,0 +1,44 @@
+package powercraft.api.world;
+
+/**
+ * @author James
+ * Locations for use in the API
+ */
+public class Location {
+	@SuppressWarnings("javadoc")
+	public int x;
+	@SuppressWarnings("javadoc")
+	public int y;
+	@SuppressWarnings("javadoc")
+	public int z;
+	
+	@SuppressWarnings("javadoc")
+	public int getX() {
+		return this.x;
+	}
+	
+	@SuppressWarnings("javadoc")
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	@SuppressWarnings("javadoc")
+	public int getY() {
+		return this.y;
+	}
+	
+	@SuppressWarnings("javadoc")
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	@SuppressWarnings("javadoc")
+	public int getZ() {
+		return this.z;
+	}
+	
+	@SuppressWarnings("javadoc")
+	public void setZ(int z) {
+		this.z = z;
+	}
+}
