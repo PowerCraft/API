e4ed6034bb24d2d420cf72f7c99bd5acd4ec0ead
zeuslightning125 <Zeusoflightning125@gmail.com>
2014-01-01 20:33:46 -0800

    Removed what I didnt need

--- powercraft/api/lists/CustomBlocksList.java
+++ powercraft/api/lists/CustomBlocksList.java
@@ -16,25 +16,15 @@ public class CustomBlocksList {
 	public Block[] blocks;
 	
 	/**
-	 * The block names list
-	 */
-	public String[] blockNames;
-	
-	/**
 	 * Adds a block to the block list
 	 * @param block The block to be added
-	 * @param name The block name to be added
 	 */
-	public void addBlock(Block block, String name){
+	public void addBlock(Block block){
 		Block[] tmp = new Block[this.blocksAmount + 1];
-		String[] tmp2 = new String[this.blocksAmount + 1];
 		for(int i = 0; i < this.blocksAmount + 1; i++){
 			tmp[i] = this.blocks[i];
-			tmp2[i] = this.blockNames[i];
 		}
 		tmp[this.blocksAmount] = block;
-		tmp2[this.blocksAmount] = name;
 		this.blocks = tmp;
-		this.blockNames = tmp2;
 	}
 }
