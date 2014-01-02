1b3934fd6b6f2a3d06bfe98918a0a03140bad86c
zeuslightning125 <Zeusoflightning125@gmail.com>
2014-01-02 10:17:17 -0800

    Initial commit

---
+++ powercraft/api/logging/PC_Exception.java
@@ -0,0 +1,33 @@
+package powercraft.api.logging;
+
+/**
+ * @author James
+ * The exeption that is thrown by PC
+ */
+public class PC_Exception extends Exception{
+
+	String issue;
+	
+	/**
+	 * The serial version UID for the excpetion (not sure what for)
+	 */
+	private static final long serialVersionUID = 8163311288878326493L;
+
+	@SuppressWarnings("javadoc")
+	public PC_Exception(String issue){
+		this.issue = issue;
+		if(issue == null || issue == ""){
+			try {
+				throw new PC_Exception("Custom thrown exception has no value!");
+			} catch (PC_Exception e) {
+				e.printStackTrace();
+				Logger.log(LogLevel.severe, e.getMessage());
+			}
+		}
+	}
+	
+	@Override
+	public String getMessage(){
+		return "Custom thrown exception in PowerCraft: " + this.issue;
+	}
+}
