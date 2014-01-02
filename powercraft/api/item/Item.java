755133926ba9eda8dbd5220605a5c83dac910d97
zeuslightning125 <Zeusoflightning125@gmail.com>
2014-01-01 20:40:22 -0800

    Added to be supportable in itemutils

--- powercraft/api/item/Item.java
+++ powercraft/api/item/Item.java
@@ -6,14 +6,19 @@ import powercraft.api.PowerCraft;
  * @author James
  * Powercraft's item
  */
-public class Item {
+public class Item extends net.minecraft.item.Item{
 		
+	@SuppressWarnings("javadoc")
+	public Item(int par1) {
+		super(par1);
+	}
+
 	// MASSIVE todo
 	
 	/**
 	 * The name of the item
 	 */
-	public String name = "";
+	public String itemName = "";
 	
 	/**
 	 * Call this, after the block is COMPLETE.
