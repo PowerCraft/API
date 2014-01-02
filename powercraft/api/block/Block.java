c3d20c94625a6cd468a5dc64d46e387d318c31ca
zeuslightning125 <Zeusoflightning125@gmail.com>
2014-01-01 17:42:07 -0800

    Spent massive amounts of time trying to figure something out; finally fixed

--- powercraft/api/block/Block.java
+++ powercraft/api/block/Block.java
@@ -7,14 +7,12 @@ import powercraft.api.stepsound.StepSoundUtil;
  * @author James
  * An independant block
  */
-public class Block {
+public class Block extends net.minecraft.block.Block{
 
 	/**
 	 * The name of the block
 	 */
 	public final String blockName;
-	
-	private net.minecraft.block.Block block;
 	
 	/**
 	 * @param id The block ID
@@ -22,30 +20,7 @@ public class Block {
 	 * @param name The name of the block
 	 */
 	public Block(int id, String material, String name){
+		super(id, MaterialUtil.getMaterialFromName(material));
 		this.blockName = name;
-		this.block = new net.minecraft.block.Block(id, MaterialUtil.getMaterialFromName(material));
-	}
-	
-	/**
-	 * @param hardness The hardness to set the block hardness to
-	 */
-	@SuppressWarnings("boxing")
-	public void setHardness(Float hardness){
-		this.block.setHardness(hardness);
-	}
-	
-	/**
-	 * @param resist The resistance to set the block hardness to
-	 */
-	@SuppressWarnings("boxing")
-	public void setResistance(Float resist){
-		this.block.setHardness(resist);
-	}
-
-	/**
-	 * @param step The name of the step sound
-	 */
-	public void setStep(String step){
-		this.block.setStepSound(StepSoundUtil.getStepSoundFromName(step));
 	}
 }
