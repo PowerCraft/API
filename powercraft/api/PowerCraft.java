150029deedcee2da180bbee2e30e9e1cff947090
zeuslightning125 <Zeusoflightning125@gmail.com>
2014-01-01 19:55:21 -0800

    Added powercraft instance used by forge for refrence

--- powercraft/api/PowerCraft.java
+++ powercraft/api/PowerCraft.java
@@ -2,6 +2,7 @@ package powercraft.api;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
@@ -14,6 +15,12 @@ import cpw.mods.fml.common.network.NetworkMod;
 @Mod(modid = "PowerCraft", name="PowerCraft", version="4.0.0")
 @NetworkMod(clientSideRequired = true, serverSideRequired = false)
 public class PowerCraft {
+	
+	/**
+	 * The powercraft instance used by forge
+	 */
+	@Instance(value="PowerCraft")
+	public static PowerCraft pc;
 	
 	/**
 	 * The preinit for powercraft
