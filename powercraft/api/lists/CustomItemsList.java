bcc57184a1e5cdd7c0e62521558d93296abc7c66
zeuslightning125 <Zeusoflightning125@gmail.com>
2014-01-01 20:33:53 -0800

    Removed what I didnt need

--- powercraft/api/lists/CustomItemsList.java
+++ powercraft/api/lists/CustomItemsList.java
@@ -16,25 +16,15 @@ public class CustomItemsList {
 	public Item[] items;
 	
 	/**
-	 * The block names list
-	 */
-	public String[] itemNames;
-	
-	/**
 	 * Adds an item to the items list
 	 * @param item The item to be added
-	 * @param name The item name to be added
 	 */
-	public void addItem(Item item, String name){
+	public void addItem(Item item){
 		Item[] tmp = new Item[this.itemsAmount + 1];
-		String[] tmp2 = new String[this.itemsAmount + 1];
 		for(int i = 0; i < this.itemsAmount + 1; i++){
 			tmp[i] = this.items[i];
-			tmp2[i] = this.itemNames[i];
 		}
 		tmp[this.itemsAmount] = item;
-		tmp2[this.itemsAmount] = name;
 		this.items = tmp;
-		this.itemNames = tmp2;
 	}
 }
