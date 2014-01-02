184304051f3ceb49f60afa395889f92326dcc8bd
zeuslightning125 <Zeusoflightning125@gmail.com>
2014-01-01 20:34:03 -0800

    Updated with register and itemName

--- powercraft/api/item/Item.java
+++ powercraft/api/item/Item.java
@@ -1,9 +1,24 @@
 package powercraft.api.item;
 
+import powercraft.api.PowerCraft;
+
 /**
  * @author James
  * Powercraft's item
  */
 public class Item {
-	// ToDo
+		
+	// MASSIVE todo
+	
+	/**
+	 * The name of the item
+	 */
+	public String name = "";
+	
+	/**
+	 * Call this, after the block is COMPLETE.
+	 */
+	public final void register(){
+		PowerCraft.pc.itemsList.addItem(this);
+	}
 }
