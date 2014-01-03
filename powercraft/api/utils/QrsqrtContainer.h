9228c6135e7389743df9a03703f9be1c8bcdfc11
zeuslightning125 <Zeusoflightning125@gmail.com>
2014-01-02 21:35:52 -0800

    Not used

---
+++ powercraft/api/utils/QrsqrtContainer.h
@@ -0,0 +1,11 @@
+#include <jni.h>
+#ifndef _Included_QrsqrtContainer
+#define _Included_QrsqrtContainer
+#ifdef __cplusplus
+extern "C" {
+#endif
+JNIEXPORT jdouble JNICALL Java_QrsqrtContainer_Q_rsqrt(JNIEnv *, jobject, jdouble);
+#ifdef __cplusplus
+}
+#endif
+#endif
