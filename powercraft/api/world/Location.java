00242db61dd797dfbcad1b26eb048d6dce260319
zeuslightning125 <Zeusoflightning125@gmail.com>
2014-01-03 08:22:33 -0800

    Removed unneeded methods

--- powercraft/api/world/Location.java
+++ powercraft/api/world/Location.java
@@ -40,34 +40,4 @@ public class Location {
 	public int y;
 	@SuppressWarnings("javadoc")
 	public int z;
-	
-	@SuppressWarnings("javadoc")
-	public int getX() {
-		return this.x;
-	}
-	
-	@SuppressWarnings("javadoc")
-	public void setX(int x) {
-		this.x = x;
-	}
-	
-	@SuppressWarnings("javadoc")
-	public int getY() {
-		return this.y;
-	}
-	
-	@SuppressWarnings("javadoc")
-	public void setY(int y) {
-		this.y = y;
-	}
-	
-	@SuppressWarnings("javadoc")
-	public int getZ() {
-		return this.z;
-	}
-	
-	@SuppressWarnings("javadoc")
-	public void setZ(int z) {
-		this.z = z;
-	}
 }
