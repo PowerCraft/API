cb8e6861dc02f6f0376cda6c76ac6d9817225b90
zeuslightning125 <Zeusoflightning125@gmail.com>
2014-01-07 17:39:16 -0800

    now uses hashmap

--- powercraft/api/lists/CustomItemsList.java
+++ powercraft/api/lists/CustomItemsList.java
@@ -1,5 +1,7 @@
 package powercraft.api.lists;
 
+import java.util.HashMap;
+
 import powercraft.api.item.PC_Item;
 
 /**
@@ -9,23 +11,16 @@ import powercraft.api.item.PC_Item;
  // TODO same things like in CustomBlocksList
 public class CustomItemsList {
 
-	private short itemsAmount = 0;
-	
 	/**
 	 * The blocks list
 	 */
-	public PC_Item[] items;
+	public HashMap<String, PC_Item> items;
 	
 	/**
 	 * Adds an item to the items list
 	 * @param item The item to be added
 	 */
 	public void addItem(PC_Item item){
-		PC_Item[] tmp = new PC_Item[this.itemsAmount + 1];
-		for(short i = 0; i < this.itemsAmount + 1; i++){
-			tmp[i] = this.items[i];
-		}
-		tmp[this.itemsAmount] = item;
-		this.items = tmp;
+		this.items.put(item.itemName, item);
 	}
 }
