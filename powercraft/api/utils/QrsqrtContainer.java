5f989486bf2a754a32e8e6360fb8882f5e064c9f
zeuslightning125 <Zeusoflightning125@gmail.com>
2014-01-07 18:00:24 -0800

    Added javadoc

--- powercraft/api/utils/QrsqrtContainer.java
+++ powercraft/api/utils/QrsqrtContainer.java
@@ -4,9 +4,16 @@ package powercraft.api.utils;
  * @author James
  * Not used for ANYTHING
  */
-@SuppressWarnings("javadoc")
 public class QrsqrtContainer {
+	/**
+	 * @param number The number to get the square root of
+	 * @return The square root of number
+	 */
 	public native double Qrsqrt(double number);
+	/**
+	 * @param number The number to get the square root of
+	 * @return The square root of number
+	 */
 	public static double Qrsqrtj(double number){
 		System.loadLibrary("C_Q_rsqrt");
 		return new QrsqrtContainer().Qrsqrt(number);
