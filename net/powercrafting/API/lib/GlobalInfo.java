3f1db1dc5c3e48e2f94af71bbc6e8cebe203f33c
zeuslightning125 <Zeusoflightning125@gmail.com>
2014-01-25 21:08:34 -0800

    You screwed up. Sorry.

--- net/powercrafting/API/lib/GlobalInfo.java
+++
@@ -1,7 +0,0 @@
-package net.powercrafting.API.lib;
-
-public class GlobalInfo {
-	public static final String modId = "powercraft";
-	public static final String fullName = "Powercraft";
-	public static final String version = "0.1.0";
-}
