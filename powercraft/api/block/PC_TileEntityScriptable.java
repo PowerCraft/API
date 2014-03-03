7c9ffe8992077010249b99fbd36685d9e08f357b
rapus95 <rapus95@gmail.com>
2014-03-03 21:28:24 +0100

    Added functionality of multi-entry-points and reverted some other changes

--- powercraft/api/block/PC_TileEntityScriptable.java
+++ powercraft/api/block/PC_TileEntityScriptable.java
@@ -1,6 +1,9 @@
 package powercraft.api.block;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import javax.script.CompiledScript;
 import javax.script.ScriptException;
@@ -25,13 +28,28 @@ public abstract class PC_TileEntityScriptable extends PC_TileEntity {
 	
 	protected ScriptException e;
 	
+	
 	public PC_TileEntityScriptable(int extSize){
 		this.ext = new int[extSize];
+		
 	}
 	
 	@SuppressWarnings("static-method")
 	protected HashMap<String, Integer> getReplacements(){
 		return null;
+	}
+	
+	@SuppressWarnings("static-method")
+	protected String[] getEntryVectors(){
+		return null;
+	}
+	
+	protected int entryIndex(String name){
+		String[] vec = getEntryVectors();
+		if(vec==null)
+			return 0;
+		int x = Arrays.asList(vec).indexOf(name);
+		return x==-1?0:x;
 	}
 	
 	@SuppressWarnings("hiding")
@@ -49,7 +67,7 @@ public abstract class PC_TileEntityScriptable extends PC_TileEntity {
 			return;
 		DiagnosticCollector<Void> diagnostic = new DiagnosticCollector<Void>();
 		try{
-			this.script = PC_Miniscript.compile(source, diagnostic, getReplacements());
+			this.script = PC_Miniscript.compile(source, diagnostic, getReplacements(), getEntryVectors());
 		}catch(ScriptException e) {
 			this.diagnostic = diagnostic;
 		}
@@ -64,12 +82,12 @@ public abstract class PC_TileEntityScriptable extends PC_TileEntity {
 	}
 	
 	@SuppressWarnings("hiding")
-	protected void invoke(){
+	protected void invoke(int entryVector){
 		if(this.e!=null || this.script==null || isClient())
 			return;
 		markDirty();
 		try{
-			PC_Miniscript.invoke(this.script, this.ext);
+			PC_Miniscript.invoke(this.script, this.ext, entryVector);
 		}catch(ScriptException e) {
 			this.e = e;
 			e.printStackTrace();
