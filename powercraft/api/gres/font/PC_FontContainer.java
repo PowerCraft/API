73adb95e857b13a649f729515d03c9566a4a41bb
zcraftler <rennsteak@gmail.com>
2014-03-04 13:28:09 +0100

    test + error fixing

--- powercraft/api/gres/font/PC_FontContainer.java
+++ powercraft/api/gres/font/PC_FontContainer.java
@@ -3,56 +3,56 @@ package powercraft.api.gres.font;
 import java.awt.Font;
 import java.io.IOException;
 import java.io.InputStream;
-
 import net.minecraft.client.renderer.texture.AbstractTexture;
 import net.minecraft.client.resources.IResource;
 import net.minecraft.client.resources.IResourceManager;
 import net.minecraft.util.ResourceLocation;
 import powercraft.api.PC_Logger;
 
-public class PC_FontContainer extends AbstractTexture{
+public class PC_FontContainer extends AbstractTexture {
 	private ResourceLocation res = null;
 	private Font font = null;
-	
-	public PC_FontContainer(){}
-	
-	public void copyFrom(PC_FontContainer fc){
+
+	public PC_FontContainer() {
+	}
+
+	public void copyFrom(PC_FontContainer fc) {
 		res = fc.res;
 		font = fc.font;
 	}
-	
-	public void setFont(Font f){
-		if(font!=null || f==null)
+
+	public void setFont(Font f) {
+		if (font != null || f == null)
 			return;
 		font = f;
 	}
-	
-	public void setResourceLocation(ResourceLocation rl){
-		if(res!=null || rl==null)
+
+	public void setResourceLocation(ResourceLocation rl) {
+		if (res != null || rl == null)
 			return;
 		res = rl;
-		
+
 	}
-	
-	public Font getFont(){
+
+	public Font getFont() {
 		return font;
 	}
-	
-	public boolean noFont(){
-		return font==null;
+
+	public boolean noFont() {
+		return font == null;
 	}
-	
-	public ResourceLocation getResourceLocation(){
+
+	public ResourceLocation getResourceLocation() {
 		return res;
 	}
-	
-	public void deriveFont(int style, float size){
+
+	public void deriveFont(int style, float size) {
 		font = font.deriveFont(style, size);
 	}
-	
+
 	@Override
-	public void loadTexture(IResourceManager resourceManager) throws IOException {
-		if(font!=null)
+	public void loadTexture(IResourceManager resourceManager) {
+		if (font != null)
 			return;
 		PC_Logger.warning("trying to read %s", res.toString());
 		InputStream inputstream = null;
@@ -64,7 +64,7 @@ public class PC_FontContainer extends AbstractTexture{
 			inputstream.close();
 			PC_Logger.warning("read %s", res.toString());
 		} catch (Exception e) { // Do not use Java 1.7, use Java 1.6
-			this.font=null;
+			this.font = null;
 		} finally {
 			if (inputstream != null)
 				try {
@@ -75,5 +75,4 @@ public class PC_FontContainer extends AbstractTexture{
 		}
 	}
 
-	
 }
