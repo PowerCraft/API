5e77f4331d189cb6b8ada68cbcdb8d7688298254
rapus95 <rapus95@gmail.com>
2014-03-15 21:59:30 +0100

    added assetsdir to params

--- ClientStart.java
+++ ClientStart.java
@@ -19,15 +19,17 @@ public class ClientStart {
 	public static void main(String args[]){
 		System.setProperty("fml.ignoreInvalidMinecraftCertificates", "true");
 		String userName = System.getProperty("user.name", "PowerCraftPlayer"+(Minecraft.getSystemTime()%1000));
+		String assetsDir="";
 		if(args!=null){
 			for(int i=0; i<args.length-1; i++){
 				if(args[i].equals("--username")){
 					userName = args[i+1];
-					break;
+				}else if(args[i].equals("--assetsDir")){
+					assetsDir = args[i+1];
 				}
 			}
 		}
-		Launch.main(new String[]{"--version", "1.6", "--tweakClass", "cpw.mods.fml.common.launcher.FMLTweaker", "--accessToken", "0", "--username", userName});
+		Launch.main(new String[]{"--version", "1.6", "--tweakClass", "cpw.mods.fml.common.launcher.FMLTweaker", "--accessToken", "0", "--username", userName, assetsDir!=""?"--assetsDir":"", assetsDir});
 	}
 	
 }
